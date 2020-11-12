70287b3dd880c9232853658248fea6f04f82ecb9
Pawel Piotrowski <pawel.piotrowski1991@gmail.com>
2020-11-12 11:14:41 +0100

    add typed payload to the PinwheelActionEvent

--- pinwheel-android/src/main/java/com/underdog_tech/pinwheel_android/model/PinwheelEvent.kt
+++ pinwheel-android/src/main/java/com/underdog_tech/pinwheel_android/model/PinwheelEvent.kt
@@ -3,6 +3,7 @@ package com.underdog_tech.pinwheel_android.model
 data class PinwheelActionEvent(
     val type: String,
     val name: String,
+    val payload: PinwheelEventPayload,
     val pageName: String,
     val actionName: String,
 )
@@ -25,6 +26,41 @@ data class PinwheelExitEvent(
     val type: String,
     val result: PinwheelResult,
     val err: PinwheelError?
+)
+
+data class PinwheelEventPayload(
+    val job: String?,
+    val mode: String?,
+    val orgName: String?,
+    val accountName: String?,
+    val accountType: String?,
+    val accountNumber: String?,
+    val routingNumber: String?,
+    val platformKey: String?,
+    val employerId: String?,
+    val amount: String?,
+    val skipIntroScreen: Boolean?,
+    val skipExitSurvey: Boolean?,
+    val skipEmployerSearch: Boolean?,
+    val apiKey: String?,
+    val employerSearchText: String?,
+    val reason: String?,
+    val otherReasonText: String?,
+    val foundFromSearch: Boolean?,
+    val inputtedProvider: String?,
+    val searchText: String?,
+    val step: String?,
+    val href: String?,
+    val label: String?,
+    val errorCode: String?,
+    val errorMsg: String?,
+    val pageName: String?,
+    val actionName: String?,
+    val platformId: String?,
+    val selectedEmployerId: String?,
+    val selectedEmployerName: String?,
+    val modalSessionId: String?,
+    val uniqueUserId: String?,
 )
 
 
