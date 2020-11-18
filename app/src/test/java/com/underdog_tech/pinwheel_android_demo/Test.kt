3ab92395d575138cf8f1a9224598f57bee351caa
Pawel Piotrowski <pawel.piotrowski1991@gmail.com>
2020-11-18 11:09:05 +0100

    add dummy tests

---
+++ app/src/test/java/com/underdog_tech/pinwheel_android_demo/Test.kt
@@ -0,0 +1,17 @@
+package com.underdog_tech.pinwheel_android_demo
+
+import org.junit.Assert
+import org.junit.Test
+
+class Test {
+
+    @Test
+    fun dummyTest() {
+        assert(true)
+    }
+
+    @Test
+    fun dummyFailTest() {
+        Assert.fail("Failed test wohoo")
+    }
+}
