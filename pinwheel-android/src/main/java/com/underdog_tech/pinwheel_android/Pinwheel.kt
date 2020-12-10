fd9a6850225fcc17f1be1932379efd4725bc34dc
Pawel Piotrowski <pawel.piotrowski1991@gmail.com>
2020-12-10 14:53:21 +0100

    add device metadata to pinwheel init event payload

--- pinwheel-android/src/main/java/com/underdog_tech/pinwheel_android/Pinwheel.kt
+++ pinwheel-android/src/main/java/com/underdog_tech/pinwheel_android/Pinwheel.kt
@@ -1,13 +1,15 @@
 package com.underdog_tech.pinwheel_android
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.webkit.WebView
+import com.underdog_tech.pinwheel_android.model.DeviceMetadata
 import com.underdog_tech.pinwheel_android.webview.PinwheelJavaScriptInterface
 import com.underdog_tech.pinwheel_android.webview.PinwheelWebViewClient
 
 object Pinwheel {
 
-    const val CDN_URL= "https://cdn.getpinwheel.com/link-v2.html"
+    const val CDN_URL = "https://cdn.getpinwheel.com/link-v2.html"
 
     fun init(webView: WebView, linkToken: String, callback: PinwheelEventListener?) {
         val timestamp = getUnixTimestamp()
@@ -20,13 +22,22 @@ object Pinwheel {
     @SuppressLint("SetJavaScriptEnabled")
     private fun configureWebView(webView: WebView, linkToken: String, uuid: String, timestamp: Long, callback: PinwheelEventListener?) {
         webView.settings.javaScriptEnabled = true
-        webView.webViewClient = PinwheelWebViewClient(linkToken, uuid, timestamp)
+        webView.webViewClient = PinwheelWebViewClient(linkToken, uuid, timestamp, getDeviceMetadata())
 
         val pinwheelJSInterface = PinwheelJavaScriptInterface(callback)
         pinwheelJSInterface.bind((webView))
 
         webView.loadUrl(CDN_URL)
     }
+
+    private fun getDeviceMetadata() = DeviceMetadata(
+        Build.VERSION.SDK_INT,
+        Build.MANUFACTURER,
+        Build.MODEL,
+        Build.PRODUCT,
+        Build.DEVICE,
+        Build.HARDWARE
+    )
 
     private fun getUnixTimestamp(): Long {
         return System.currentTimeMillis()
--- pinwheel-android/src/main/java/com/underdog_tech/pinwheel_android/webview/PinwheelWebViewClient.kt
+++ pinwheel-android/src/main/java/com/underdog_tech/pinwheel_android/webview/PinwheelWebViewClient.kt
@@ -2,8 +2,9 @@ package com.underdog_tech.pinwheel_android.webview
 
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.underdog_tech.pinwheel_android.model.DeviceMetadata
 
-class PinwheelWebViewClient(private val linkToken: String, private val uuid: String, private val timestamp: Long): WebViewClient() {
+class PinwheelWebViewClient(private val linkToken: String, private val uuid: String, private val timestamp: Long, private val metaData: DeviceMetadata): WebViewClient() {
 
     override fun onPageFinished(view: WebView?, url: String?) {
         view?.let { injectJS(it) }
@@ -21,14 +22,22 @@ class PinwheelWebViewClient(private val linkToken: String, private val uuid: Str
                                 payload: {
                                     fullScreen: true,
                                     sdk: 'android',
+                                    deviceMetadata: {
+                                        os: ${metaData.os},
+                                        manufacturer: '${metaData.manufacturer}',
+                                        model: '${metaData.model}',
+                                        product: '${metaData.product}',
+                                        device: '${metaData.device}',
+                                        hardware: '${metaData.hardware}',
+                                    },
                                     initializationTimestamp: $timestamp,
                                     linkToken: '$linkToken',
                                     uniqueUserId: '$uuid',
                                  }
                             }
                   );
-                  } catch(err) { 
-                    console.error(err); 
+                  } catch(err) {
+                    console.error(err);
                   }"""
 
         view.loadUrl("javascript:(function f() {$script})()")
